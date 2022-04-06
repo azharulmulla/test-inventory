dce894d8cd81534bfd2fc310957cbf4e2a5f6510
azharulmulla <azharul@shorepointsys.com>
2022-04-07 02:06:55 +0600

    test file extended

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -1,17 +1,56 @@
+
 import unittest
 import logging
+from ..models.picking import Move
+from ..models.picking import Picking
+from ..models.res_company import ResCompany
 
 _logger = logging.getLogger(__name__)
 
-class DefaultIntervalTimeTestCase(unittest.TestCase):
-    def test_default_interval_time(self):
-        default_time = self.env['ir.cron'].search([('interval_number', '==', 89),])
-        self.assertEqual(default_time.size(), (89, 89))
+class TestInventory(unittest.TestCase):
 
-        _logger.info("&&&&&&&&&&&&&&&&&&&&&&&&")
-        _logger.info(default_time)
-        _logger.info("&&&&&&&&&&&&&&&&&&&&&&&&")
+    def setUp(self):
+        print("setup")
+        super(TestInventory, self).setUp()
 
-    # def test_stock_move_time(self):
-    #     move_time = self.env['ir.cron'].search([])
-    #     if move_time > 89:
+        self.interval_time = self.env.ref('base.model_ir_cron')
+        self.interval_time.write({'interval_number': 89})
+
+    def tearDown(self):
+        print("tearDown\n")
+
+
+    def test_intervalNumber(self):
+        time = self.interval_time
+        self.assertEqual(time, 89)
+
+        _logger.info("****************************************")
+
+
+    def test_stock_move_validation(self):
+        company = self.env.company.consignee_company_ids
+        for compny in company:
+            compny.with_context(comp_check=compny).update_stock()
+
+        _logger.info(compny) 
+
+    def test_consignee_stock(self):
+        products = self.env['product.template']
+
+        product_vals = {'name': 10103-1343,
+                    'sale_ok': True,
+                    'purchase_ok': True,
+                    'is_garment': True,
+                    'type': self.selection.product}
+
+        company = self.env.company.consignee_company_ids
+
+        if product_vals in products:
+            if company in product_vals:
+                move = company.with_context(comp_check=company).update_stock()
+                _logger.info(move)
+  
+
+            
+if __name__ == '__main__':
+    unittest.main()  
